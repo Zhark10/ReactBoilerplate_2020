2f2fde332cc26512576714bfe6aa143d1dd01e66
Zhark10 <arkady.zharavin@citronium.biz>
2020-05-30 20:56:46 +0300

    init, entry point, etc

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+const app = (
+    <div>
+        Hello, world!
+    </div>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
