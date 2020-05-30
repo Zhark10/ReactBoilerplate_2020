bffd73359eb887d1f464a95e4cf069340f9dd602
Zhark10 <arkady.zharavin@citronium.biz>
2020-05-31 02:10:05 +0300

    added main page

---
+++ src/ui/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.scss';
+
+export default function Home() {
+    return (
+        <div className="main-page">
+            is main page
+        </div>
+    )
+}
