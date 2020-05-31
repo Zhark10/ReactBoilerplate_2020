ffc8105e53d44bbcf836e3a3b48ceea6ca5f7720
Zhark10 <arkady.zharavin@citronium.biz>
2020-06-01 00:09:16 +0300

    minor changes

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,19 +3,19 @@ import { Router, Route, Switch } from 'react-router-dom';
 import { history } from '../redux/config/store';
 import { Shared } from '../ui/shared/exports';
 
-const HomePage = React.lazy(() => import('../ui/pages/Home/Home'));
-
 export default class MainRouter extends Component {
-
+    
     render() {
         return (
             <Router history={history}>
                 <Suspense fallback={<Shared.Preloader />}>
                     <Switch>
-                        <Route path="/" component={HomePage} />
+                        <Route path="/" component={_Home} />
                     </Switch>
                 </Suspense>
             </Router>
         );
     }
 }
+
+const _Home = React.lazy(() => import('../ui/pages/Home/Home'));
