dd0f8b5acdc8b26f2dac5081504fb241fdc139b5
Zhark10 <verygoodman18@yandex.ru>
2020-06-12 01:28:34 +0300

    exports

--- src/redux/config/root-reducer.ts
+++ src/redux/config/root-reducer.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import * as Reducers from '../modules/exports'
 
 const rootReducer = combineReducers<any>({
+    ...Reducers,
     router: routerReducer,
 });
 
