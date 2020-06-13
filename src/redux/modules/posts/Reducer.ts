bc08d5f934c3e520661c112841080b9b11fd618f
Zhark10 <verygoodman18@yandex.ru>
2020-06-13 21:09:12 +0300

    post reducer created

--- src/redux/modules/posts/Reducer.ts
+++ src/redux/modules/posts/Reducer.ts
@@ -1,11 +1,17 @@
 import { handleActions } from 'redux-actions';
-import { IPosts } from './Types';
+import { IStatePosts, IPost } from './Types';
 import { ActionsPosts } from './Actions';
 
-const initialState = null;
+const initialState: IStatePosts = {
+    isLoading: false,
+    data: null,
+};
 
-const ReducerPosts = handleActions<IPosts | null, IPosts>({
-    [ActionsPosts.Type.SAVE_POSTS]: (state, action) => action.payload,
+const ReducerPosts = handleActions<IStatePosts, IPost[]>({
+    [ActionsPosts.Type.IS_LOADER_SHOW]: (state) => ({...state, isLoading: true}),
+    [ActionsPosts.Type.IS_LOADER_HIDE]: (state) => ({...state, isLoading: false}),
+    
+    [ActionsPosts.Type.SAVE_POSTS]: (state, action) => ({...state, data: action.payload}),
 }, initialState);
 
 export default ReducerPosts
--- src/redux/modules/posts/Types.ts
+++ src/redux/modules/posts/Types.ts
@@ -1,3 +1,11 @@
-export interface IPosts {
+export interface IStatePosts {
+  isLoading: boolean,
+  data: IPost[] | null,
+}
 
+export interface IPost {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
 }
