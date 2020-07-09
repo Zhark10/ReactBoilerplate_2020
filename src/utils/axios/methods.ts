a404d4d6b63ac78bdf2aa88026b60d8dfd9a7d44
Zhark10 <verygoodman18@yandex.ru>
2020-07-09 18:22:10 +0300

    updated saga

--- src/utils/axios/methods.ts
+++ src/utils/axios/methods.ts
@@ -1,24 +1,35 @@
 import axios from 'axios'
+import { TResponseModel, TStoreTemplate } from 'redux/config/root-types'
+import { DataConverter } from './dataConverter'
 
-export class RequestsMethods {
-  private validToken = () => {
-    return localStorage.getItem('token')
-  }
-  private API_URL = process.env.REACT_APP_API_URL
+const { convertToStoreModel } = DataConverter()
 
-  config = { headers: { Authorization: 'Bearer ' + this.validToken() } }
-
-  post = async (url: string, data: any): Promise<any> => {
-    return await axios.post(this.API_URL + url, data, this.config)
-  }
-
-  get = async (url: string): Promise<any> => {
-    return await axios.get(this.API_URL + url, this.config)
-  }
-
-  put = async (url: string, data: any): Promise<any> => {
-    return await axios.put(this.API_URL + url, data, this.config)
-  }
+const validToken = () => {
+  return localStorage.getItem('token')
 }
 
-export const Axios = new RequestsMethods()
+export const RequestMethods = () => {
+  const API_URL = process.env.REACT_APP_API_URL
+  const config = !!validToken() ? { headers: { Authorization: 'Bearer ' + validToken() } } : {}
+
+  async function post<T>(url: string, data: any): Promise<TStoreTemplate<T>> {
+    const response = await axios.post(API_URL + url, data, config)
+    return convertToStoreModel(response.data as TResponseModel<T>)
+  }
+
+  async function get<T>(url: string): Promise<TStoreTemplate<T>> {
+    const response = await axios.get(API_URL + url, config)
+    return convertToStoreModel(response.data as TResponseModel<T>)
+  }
+
+  async function put<T>(url: string, data: any): Promise<TStoreTemplate<T>> {
+    const response = await axios.put(API_URL + url, data, config)
+    return convertToStoreModel(response.data as TResponseModel<T>)
+  }
+
+  return {
+    post,
+    get,
+    put,
+  }
+}
