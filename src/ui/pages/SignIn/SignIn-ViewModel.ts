6b3917824cb9ccf8549c165479b790b6ca84001c
Zhark10 <verygoodman18@yandex.ru>
2020-07-20 23:57:32 +0300

    updated readme

--- src/ui/pages/SignIn/SignIn-ViewModel.ts
+++ src/ui/pages/SignIn/SignIn-ViewModel.ts
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form'
 
 export const useSignIn = () => {
   const { handleSubmit, register, errors } = useForm()
-  const onSubmit = (values) => console.log(values)
+  const onSubmit = (values: any) => console.log(values)
 
   return {
     form: {
