ed050bde15bf48618b2bcc0139f254fe2c12cd81
Zhark10 <verygoodman18@yandex.ru>
2022-02-21 22:26:33 +0300

    fix versions for build

--- src/redux/config/store.ts
+++ src/redux/config/store.ts
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import createSagaMiddleware from 'redux-saga'
-import { createBrowserHistory } from 'history'
+import { createBrowserHistory, History } from 'history'
 
 import rootSaga from './root-saga'
 import rootReducer from './root-reducer'
 import { errorHandler } from './root-error-handler'
 
-const history = createBrowserHistory()
+const history: History = createBrowserHistory()
 const sagaMiddleware = createSagaMiddleware({
   onError: errorHandler,
 })
