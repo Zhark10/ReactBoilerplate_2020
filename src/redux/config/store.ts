def6524272dd32461449d9334cc29c98482612a3
Zhark10 <verygoodman18@yandex.ru>
2022-02-22 18:18:44 +0300

    upd react version

--- src/redux/config/store.ts
+++ src/redux/config/store.ts
@@ -1,19 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import createSagaMiddleware from 'redux-saga'
-import { createBrowserHistory, History } from 'history'
+import { createBrowserHistory } from 'history'
 
 import rootSaga from './root-saga'
 import rootReducer from './root-reducer'
 import { errorHandler } from './root-error-handler'
 
-const history: History = createBrowserHistory()
+const history = createBrowserHistory({
+  basename: '/',
+})
 const sagaMiddleware = createSagaMiddleware({
   onError: errorHandler,
 })
 
 const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware, routerMiddleware(history)))
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware, routerMiddleware(history as any)))
 
 const store = createStore(rootReducer, enhancer)
 sagaMiddleware.run(rootSaga)
